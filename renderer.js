bc121df3cca884cb0fed861e37078b34818d6f96
myztajay <myztajay@hotmail.com>
2017-04-11 11:17:26 -0400

    Added ability to create template based on input

--- renderer.js
+++ renderer.js
@@ -13,7 +13,44 @@
 
 const newLinkUrl = document.querySelector('#new-link-url');
 const newLinkSubmit = document.querySelector('.new-link-form--submit');
+const newLinkForm = document.querySelector('.new-link-form');
+const linkTemplate = document.querySelector('#link-template');
+const linksSection = document.querySelector('.links');
 
 newLinkUrl.addEventListener('keyup', ()=>{
   newLinkSubmit.disabled = !newLinkUrl.validity.valid;
+});
+
+const parser = new DOMParser()
+const parseResponse = (text) => parser.parseFromString(text, 'text/html');
+const findTitle = (nodes) => nodes.querySelector('title');
+
+const addToPage = ({ title, url }) => {
+
+  const newLink = linkTemplate.content.cloneNode(true);
+  const titleElement = newLink.querySelector('.link--title');
+  const urlElement = newLink.querySelector('.link--url')
+
+  titleElement.textContent = title;
+  urlElement.href = url;
+  urlElement.textContent = url;
+
+  linksSection.appendChild(newLink);
+  return { title, url}
+}
+
+newLinkForm.addEventListener('submit', ()=>{
+  event.preventDefault();
+
+  const url = newLinkUrl.value;
+
+  fetch(url)
+    .then(response => response.text())
+    .then(parseResponse)
+    .then(findTitle)
+    .then(title => ({ title, url}))
+    .then(response => console.log(response) )
+    .then(addToPage)
+    .then(title => console.log(title))
+    .catch( (error) => console.error(error))
 });
